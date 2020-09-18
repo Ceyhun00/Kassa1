cae2ea5701e850e041831949802f2ce0244f6443
Джейхун Ахмедов <lezgin5.5@mail.ru>
2020-09-18 22:39:07 +0300

    Logic Update

--- KassaOne/Controllers/KassaController.cs
+++ KassaOne/Controllers/KassaController.cs
@@ -1,13 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using KassaOne.Models;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.VisualBasic.CompilerServices;
 
 namespace KassaOne.Controllers
 {
@@ -23,41 +17,21 @@ namespace KassaOne.Controllers
             return View();
         }
 
-        public ShowModel ParseModel(CreditViewModel creditModel)
-        {
-            ShowModel showModel = new ShowModel();
-            var a = CalculateShowModel(creditModel);
-            showModel.PurchasePrice = creditModel.CreditSum.ToString("N2");
-            showModel.CreditTime = creditModel.CreditTime.ToString();
-            showModel.MonthlyPayment = a.ToString("N2");
-            showModel.CreditSum = (creditModel.CreditTime * a).ToString("N2");
-            showModel.TotalOverpayment = ((creditModel.CreditTime * a) - double.Parse(showModel.PurchasePrice)).ToString("N2");
-            return showModel;
-        }
-        public double CalculateShowModel(CreditViewModel creditModel)
-        {
-            double s = creditModel.CreditSum;
-            double i = (creditModel.RatePerYear / 12) / 100;
-            double n = creditModel.CreditTime;
-            double k = (i * Math.Pow((1 + i), n)) / (Math.Pow((1 + i), n) - 1);
-            double a = k * s;
-            return a;
-        }
-
         [HttpPost]
         public IActionResult Index(CreditViewModel creditModel)
         {
             creditModel.CreditTime *= creditModel.OptionValue;
-            var showModel = ParseModel(creditModel);
-            var showTable = PaymentScheduleAnnuitet(creditModel.CreditSum,
+            var showModel = ParseToShowModel(creditModel);
+            var showTable = AnnualPaymentSchedule(creditModel.CreditSum,
                 creditModel.RatePerYear,
                 creditModel.CreditTime,
                 double.Parse(showModel.MonthlyPayment));
+
             var obj = new ResultViewModel (showModel,showTable );
             return View($"Index2",obj);
         }
 
-        protected List<ShowTable> PaymentScheduleAnnuitet(double creditSum, double ratePerYear, double creditTime, double payment) // Метод расчета Аннуитетного платежа
+        protected List<ShowTable> AnnualPaymentSchedule(double creditSum, double ratePerYear, double creditTime, double payment) // Метод расчета Аннуитетного платежа
         {
             var tableLst = new List<ShowTable>();
             for (int i = 0; i < creditTime; ++i)
@@ -70,11 +44,28 @@ namespace KassaOne.Controllers
                 var payPercent = percentage.ToString("N2"); //Платеж процента
                 var remainingSum = creditSum.ToString("N2"); //Основной остаток
                 tableLst.Add(new ShowTable(monthNum, monthlyPayment, payMain, payPercent, remainingSum));
-
             }
             return tableLst;
         }
-     
+        public ShowModel ParseToShowModel(CreditViewModel creditModel)
+        {
+            ShowModel showModel = new ShowModel();
+            var annuityCoefficient = CalculateAnnuity(creditModel);
+            showModel.PurchasePrice = creditModel.CreditSum.ToString("N2");
+            showModel.CreditTime = creditModel.CreditTime.ToString();
+            showModel.MonthlyPayment = annuityCoefficient.ToString("N2");
+            showModel.CreditSum = (creditModel.CreditTime * annuityCoefficient).ToString("N2");
+            showModel.TotalOverpayment = ((creditModel.CreditTime * annuityCoefficient) - double.Parse(showModel.PurchasePrice)).ToString("N2");
+            return showModel;
+        }
+        public double CalculateAnnuity(CreditViewModel creditModel)
+        {
+            double monthlyRate = (creditModel.RatePerYear / 12) / 100;
+            double periodNum = creditModel.CreditTime;
+            double annuityCoefficient = (monthlyRate * Math.Pow((1 + monthlyRate), periodNum)) / (Math.Pow((1 + monthlyRate), periodNum) - 1);
+            double monthlyAnnuityPayment = annuityCoefficient * creditModel.CreditSum;
+            return monthlyAnnuityPayment;
+        }
 
     }
 }
